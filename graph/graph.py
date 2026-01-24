260c8143db0e57ddde6167d350579f19d9e59a1c
TkWarrior <talibulhaque369@gmail.com>
2026-01-24 17:59:29 +0530

    added graph

---
+++ graph/graph.py
@@ -0,0 +1,27 @@
+from langgraph.graph import StateGraph
+from states.states import DebateState
+from agents.pro_agent import pro_agent
+from agents.conn_agent import conn_agent
+from agents.judge_agent import judge_agent
+from agents.summary_agent import summary_agent
+
+graph = StateGraph(DebateState)
+
+graph.set_entry_point("pro_agent")  ## Starting point of the graph
+graph.add_node("pro_agent", pro_agent)
+graph.add_node("conn_agent", conn_agent)    
+graph.add_node("judge_agent", judge_agent)
+graph.add_node("summary_agent", summary_agent)
+
+graph.add_edge("pro_agent", "conn_agent")
+graph.add_edge("conn_agent", "judge_agent")
+graph.add_edge("judge_agent", "summary_agent")
+graph.add_edge("summary_agent", graph.END)
+graph.add_conditional_edges(
+    "judge_agent",
+    should_continue=lambda state: "summary_agent" if state["debate_over"] else "pro_agent",
+    transitions = {"pro_agent": "pro_agent", "summary_agent": "summary_agent"}
+)
+
+agent = graph.compile()
+print(agent)
